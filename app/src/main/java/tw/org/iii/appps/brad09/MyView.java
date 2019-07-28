f9c1bef285fa597de09af13801ebac0d3751e1a8
Brad <brad@brad.tw>
2019-07-28 15:58:54 +0800

    02

--- app/src/main/java/tw/org/iii/appps/brad09/MyView.java
+++ app/src/main/java/tw/org/iii/appps/brad09/MyView.java
@@ -9,19 +9,24 @@ import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyView extends View {
     private Resources res;
     private float viewW, viewH;
     private boolean isInit;
     private Bitmap ballBmp;
-    private float ballW, ballH, ballX, ballY;
+    private float ballW, ballH, ballX, ballY, dx, dy;
     private Matrix matrix;
+    private Timer timer;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         res = context.getResources();
         matrix = new Matrix();
+        timer = new Timer();
     }
 
     private void init(){
@@ -35,7 +40,12 @@ public class MyView extends View {
             ballBmp.getWidth(), ballBmp.getHeight(), matrix, false);
 
         ballX = ballY = 10;
+        dx = dy = 8;
 
+        setBackgroundResource(R.drawable.bg);
+
+        timer.schedule(new refreshView(), 0, 17);
+        timer.schedule(new BallTask(), 1*1000, 30);
 
         isInit = true;
     }
@@ -48,4 +58,28 @@ public class MyView extends View {
         canvas.drawBitmap(ballBmp, ballX , ballY, null);
 
     }
+
+    private class BallTask extends TimerTask {
+        @Override
+        public void run() {
+            if (ballX < 0 || ballX + ballW > viewW){
+                dx *= -1;
+            }
+            if (ballY < 0 || ballY + ballH > viewH){
+                dy *= -1;
+            }
+
+
+            ballX += dx;
+            ballY += dy;
+        }
+    }
+
+    private class refreshView extends TimerTask {
+        @Override
+        public void run() {
+            postInvalidate();
+        }
+    }
+
 }
